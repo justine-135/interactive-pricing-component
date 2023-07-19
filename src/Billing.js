b264dfdf3176cea4ea2a2af4473dbd849cbdc1e3
Justine <ujusine@yahoo.com>
2023-07-19 18:55:16 +0800

    add toggle button function, and slider bar to adjust price for pageviews

--- src/Billing.js
+++ src/Billing.js
@@ -1,11 +1,86 @@
+import { useState } from "react";
+import { ReactComponent as Check } from "./images/icon-check.svg";
+
 const Billing = () => {
+  const [toggle, setToggle] = useState(false);
+  const [bar, setBar] = useState(3);
+  const [pageViews] = useState({
+    1: { views: 10000, cost: 8.0 },
+    2: { views: 50000, cost: 12.0 },
+    3: { views: 100000, cost: 16.0 },
+    4: { views: 500000, cost: 24.0 },
+    5: { views: 1000000, cost: 36.0 },
+  });
+
+  const handleToggle = () => {
+    toggle ? setToggle(false) : setToggle(true);
+  };
+
+  const handleSliderBar = (e) => {
+    setBar(e.target.value);
+  };
+
+  function kFormatter(num) {
+    return num >= 1000 && num < 1000000
+      ? (num / 1000).toFixed(1).replace(/\.0$/, "") + "K"
+      : num >= 1000000 && num < 1000000000
+      ? (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M"
+      : num >= 1000000000 && num < 1000000000000
+      ? (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "B"
+      : num >= 1000000000000 && num < 1000000000000000
+      ? (num / 1000000000000).toFixed(1).replace(/\.0$/, "") + "T"
+      : num;
+  }
+
   return (
     <section>
       <div className="billing-container">
-        <div className="pageview-container">
-          <p className="pageviews">100K PAGEVIEWS</p>
-          <div></div>
-          <div></div>
+        <div className="grid-container">
+          <p className="grid1">{kFormatter(pageViews[bar].views)} PAGEVIEWS</p>
+          <div className="grid2">
+            <div className="slidecontainer">
+              <input
+                type="range"
+                min="1"
+                max="5"
+                value={bar}
+                onChange={(e) => {
+                  handleSliderBar(e);
+                }}
+              />
+            </div>
+          </div>
+          <div className="grid3">
+            <span>${pageViews[bar].cost.toFixed(2)}</span>
+            <span> / month</span>
+          </div>
+          <div className="grid4">
+            <span>Monthly Billing</span>
+            <button
+              className={`switch-btn ${toggle ? "on" : "off"}`}
+              onClick={handleToggle}
+            >
+              <span className={toggle ? "on" : "off"}></span>
+            </button>
+            <div>
+              <span>Yearly Billing</span>
+              <span>-25%</span>
+            </div>
+          </div>
+        </div>
+        <div className="trial-container">
+          <ul>
+            <li>
+              <Check className="svg" /> Unlimited websites
+            </li>
+            <li>
+              <Check className="svg" /> 100% data ownership
+            </li>
+            <li>
+              <Check className="svg" /> Email reports
+            </li>
+          </ul>
+          <button>Start my trial</button>
         </div>
       </div>
     </section>
