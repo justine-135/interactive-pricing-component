1d5aa466e18d42b36e5bc762ee71e6b0f611950d
Justine <ujusine@yahoo.com>
2023-07-19 15:13:28 +0800

    render billing component

---
+++ src/Billing.js
@@ -0,0 +1,15 @@
+const Billing = () => {
+  return (
+    <section>
+      <div className="billing-container">
+        <div className="pageview-container">
+          <p className="pageviews">100K PAGEVIEWS</p>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Billing;
